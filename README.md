0f5cc4c7eb899c1fdedbfbca369a0e4980137c8f
Raynier Yap <raynieryap@gmail.com>
2024-02-28 15:39:06 +1100

    fixed formating and added documentation

--- README.md
+++ README.md
@@ -6,17 +6,17 @@ This project fetches the latest COVID-19 data from Our World in Data, filters th
 
 Before running the script, you need to install the required Python libraries. You can do this by running the following command:
 
-\```bash
+```plaintext
 pip install pandas altair requests
-\```
+```
 
 ## Usage
 
 To run the script, navigate to the project directory and run the following command:
 
-\```bash
+```plaintext
 python main.py
-\```
+```
 
 This will create a line chart and save it as an HTML file named `daily-covid-cases.html` in the project directory.
 
--- main.py
+++ main.py
@@ -3,50 +3,110 @@ import altair as alt
 import requests
 import os
 
-# Define the URL of the CSV file
-url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
+def download_covid_data():
+    """
+    Downloads the daily COVID-19 cases and deaths data from a URL,
+    saves it to a CSV file, and returns the data as a pandas DataFrame.
 
-# Send a HTTP request to the URL
-response = requests.get(url)
+    Returns:
+    df (pandas.DataFrame): DataFrame containing the daily COVID-19 cases and deaths data.
+    """
+    # Define the URL of the CSV file
+    url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
 
-# Make sure the request was successful
-assert response.status_code == 200, 'Failed to download the file'
+    # Send a HTTP request to the URL
+    response = requests.get(url)
 
-# Write the content of the response to a file
-with open('daily-covid-cases-deaths.csv', 'wb') as file:
-    file.write(response.content)
+    # Make sure the request was successful
+    assert response.status_code == 200, 'Failed to download the file'
 
-# Load the data into a pandas DataFrame
-df = pd.read_csv('daily-covid-cases-deaths.csv')
+    # Write the content of the response to a file
+    with open('daily-covid-cases-deaths.csv', 'wb') as file:
+        file.write(response.content)
 
-# Filter the data to include only the desired columns
-df = df[['location', 'date', 'new_cases_smoothed']]
+    # Load the data into a pandas DataFrame
+    df = pd.read_csv('daily-covid-cases-deaths.csv')
 
-# Filter the data to include only the last 50 days of data
-df['date'] = pd.to_datetime(df['date'])
-df = df[df['date'] >= df['date'].max() - pd.Timedelta(days=50)]
+    return df
 
-# Filter the data to include only 10 countries
-countries = ['United States', 'India', 'Brazil', 'United Kingdom', 'Russia', 'France', 'Turkey', 'Italy', 'Spain', 'Germany']
-df = df[df['location'].isin(countries)]
+def filter_covid_data(df):
+    """
+    Filters the COVID-19 cases and deaths data to include only the desired columns,
+    the last 50 days of data, and data for 10 specific countries.
 
-chart = alt.Chart(df).mark_line().encode(
-    x=alt.X('date', title='Date'),
-    y=alt.Y('new_cases_smoothed', title='New Cases'),
-    color=alt.Color('location', title='Country')
-).properties(
-    title='Daily New COVID-19 Cases',
-    width=1000,
-    height=600
-).configure_axis(
-    labelFontSize=12,
-    titleFontSize=14
-).configure_title(
-    fontSize=16
-)
+    Args:
+    df (pandas.DataFrame): DataFrame containing the daily COVID-19 cases and deaths data.
 
-# Save the chart to an HTML file
-chart.save('daily-covid-cases.html')
-#delete the csv file but keep the html file
-os.remove('daily-covid-cases-deaths.csv')
-os.remove('filtered-daily-covid-cases-deaths.csv')
+    Returns:
+    df (pandas.DataFrame): Filtered DataFrame containing the desired COVID-19 data.
+    """
+    # Filter the data to include only the desired columns
+    df = df[['location', 'date', 'new_cases_smoothed']]
+
+    # Filter the data to include only the last 50 days of data
+    df['date'] = pd.to_datetime(df['date'])
+    df = df[df['date'] >= df['date'].max() - pd.Timedelta(days=50)]
+
+    # Filter the data to include only 10 countries
+    countries = ['United States', 'India', 'Brazil', 'United Kingdom', 'Russia', 'France', 'Turkey', 'Italy', 'Spain', 'Germany']
+    df = df[df['location'].isin(countries)]
+
+    return df
+
+def create_chart(df):
+    """
+    Creates an Altair line chart based on the filtered COVID-19 data.
+
+    Args:
+    df (pandas.DataFrame): Filtered DataFrame containing the desired COVID-19 data.
+
+    Returns:
+    chart (altair.Chart): Altair line chart representing the daily new COVID-19 cases.
+    """
+    chart = alt.Chart(df).mark_line().encode(
+        x=alt.X('date', title='Date'),
+        y=alt.Y('new_cases_smoothed', title='New Cases'),
+        color=alt.Color('location', title='Country')
+    ).properties(
+        title='Daily New COVID-19 Cases',
+        width=1000,
+        height=600
+    ).configure_axis(
+        labelFontSize=12,
+        titleFontSize=14
+    ).configure_title(
+        fontSize=16
+    )
+
+    return chart
+
+def save_chart(chart):
+    """
+    Saves the Altair chart as an HTML file.
+
+    Args:
+    chart (altair.Chart): Altair line chart representing the daily new COVID-19 cases.
+    """
+    # Save the chart to an HTML file
+    chart.save('daily-covid-cases.html')
+
+def delete_csv_file():
+    """
+    Deletes the CSV file containing the daily COVID-19 cases and deaths data.
+    """
+    os.remove('daily-covid-cases-deaths.csv')
+
+# Download the COVID-19 data
+df = download_covid_data()
+
+# Filter the COVID-19 data
+df = filter_covid_data(df)
+
+# Create the chart
+chart = create_chart(df)
+
+# Save the chart as an HTML file
+save_chart(chart)
+
+# Delete the CSV file
+delete_csv_file()
